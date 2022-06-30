99b1d49065ddf22a7043c683a4b4a0bace1c7ce1
Joao Roberto <joaocte@gmail.com>
2022-06-30 06:41:09 -0300

    change deletebookusecase

--- src/main/kotlin/com/mercadolivro/application/usecase/book/deleteBook/DeleteBookUseCase.kt
+++ src/main/kotlin/com/mercadolivro/application/usecase/book/deleteBook/DeleteBookUseCase.kt
@@ -1,17 +1,23 @@
 package com.mercadolivro.application.usecase.book.deleteBook
 
 import com.mercadolivro.application.command.DeleteBookByIdCommand
+import com.mercadolivro.infrastructure.model.BookModel
+import com.mercadolivro.infrastructure.model.BookStatusModel
 import com.mercadolivro.infrastructure.repository.IBookRepository
 import org.springframework.stereotype.Service
 
 @Service
 class DeleteBookUseCase (val repository: IBookRepository) :IDeleteBookUseCase {
     override fun execute(deleteBookCommand: DeleteBookByIdCommand) {
-        val bookExists = repository.existsById(deleteBookCommand.id)
+        val book = repository.findById(deleteBookCommand.id)
 
-        if (!bookExists)
+        if (book.isEmpty)
         throw Exception("Book not found")
 
-        repository.deleteById(deleteBookCommand.id)
+        val bookModel = book.get()
+
+        bookModel.status = BookStatusModel.CANCELADO
+
+        repository.save(bookModel)
     }
 }
